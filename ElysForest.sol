048e8247292820360780d05968a9fa6881a7b42c
nuwrldnf8r <nuwrldnf8r@gmail.com>
2021-09-27 13:10:22 +0200

    updated forest lock with donation

---
+++ ElysForest.sol
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./ElysLockNFT.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract ForestFactory is Ownable{
+    using SafeERC20 for IERC20;
+
+    // ERC20 basic token contract being held
+    IERC20 private _token;
+    
+    LockedELYS private immutable _lockedElys;
+    
+    uint256 private _tokenIdCounter = 0;
+    
+    address private _donationAddress;
+    
+
+    constructor(IERC20 token_,address donationAddress){
+        _token = token_;
+        _lockedElys = new LockedELYS(token_);
+        _donationAddress = donationAddress;
+    }
+    
+    function changeDonation(address donationAddress) public onlyOwner{
+        _donationAddress = donationAddress;
+    }
+    
+    function lockNFT() public view returns(LockedELYS){
+        return _lockedElys;
+    }
+    
+    function _addDec(uint256 amount) public pure returns (uint256){
+        return amount * (10**5);
+    }
+    
+    function getReward(uint256 lockDays, uint256 amount) public view returns (uint256){
+        
+        //work out reward
+        uint256 perc = 0;
+
+        if(lockDays>=7 && lockDays<14)perc = 4;
+        if(lockDays>=14 && lockDays<28)perc = 5;
+        if(lockDays>=28 && lockDays<3*28)perc = 6;
+        if(lockDays>=3*28 && lockDays<6*28)perc=9;
+        if(lockDays>=6*28 && lockDays<9*28)perc=12;
+        if(lockDays>=9*28 && lockDays<365)perc=15;
+        if(lockDays>=365 && lockDays<2*365)perc=20;
+        if(lockDays>=2*365 && lockDays<3*365)perc=23;
+        if(lockDays>=3*365)perc=26;
+        
+        uint256 rewardAmount = amount*perc*lockDays/36500;
+        if(_token.balanceOf(address(this))>rewardAmount) return rewardAmount;
+        return 0;
+    }
+    
+    function lock(uint256 amount, uint256 lockDays, uint256 donation, uint256 tokenId) public {
+        uint256 reward = getReward(lockDays, amount);
+        uint256 donationAmount;
+        if(donation>0){
+            donationAmount = donation/reward * 100;
+            reward -= donationAmount;
+        }
+        require(_token.allowance(msg.sender, address(this))>=amount);
+        _token.safeTransferFrom(msg.sender,address(_lockedElys),amount);
+        if(reward>0)_token.safeTransfer(address(_lockedElys),reward);
+        if(donationAmount>0)_token.safeTransfer(_donationAddress,donationAmount);
+        _lockedElys.mint(msg.sender, tokenId, lockDays, amount, reward);
+        _tokenIdCounter++;
+    }
+    
+    function tokenIdCounter() public view returns (uint256) {
+        return _tokenIdCounter;
+    }
+    
+
+   
+}
--- ElysLockNFT.sol
+++ ElysLockNFT.sol
@@ -1,25 +1,29 @@
-// contracts/GLDToken.sol
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract LockedELYS is ERC721, Ownable {
+contract LockedELYS is ERC721Enumerable, Ownable {
     using SafeERC20 for IERC20;
     
     string private _baseUri;
     IERC20 private immutable _token;
+    address private immutable _factory;
     
     mapping(uint256 => uint256) private _lockedAmount; //maps tokenID to amount locked for that token
     mapping(uint256 => uint256) private _reward;
     mapping(uint256 => uint256) private _lockDays; //maps tokenID to num days locked
     mapping(uint256 => uint256) private _lockStart; //maps tokenID to start of lock
     
+    uint256 private _timestamp;
+    
     constructor(IERC20 token) ERC721("Locked Elys", "LELYS"){
        _baseUri = "";
        _token = token;
+       _factory = msg.sender;
+       _timestamp = block.timestamp;
     }
     
     function _baseURI()  internal override view returns (string memory) {
@@ -31,7 +35,7 @@ contract LockedELYS is ERC721, Ownable {
         require(lockDays>0 && amount>0);
         _lockedAmount[tokenId] = amount;
         _reward[tokenId] = reward;
-        _lockStart[tokenId] = block.timestamp;
+        _lockStart[tokenId] = _blockTime();
         _lockDays[tokenId] = lockDays;
     }
     
@@ -39,18 +43,18 @@ contract LockedELYS is ERC721, Ownable {
         return _lockedAmount[tokenId];
     }
     
-    function reward(uint256 tokenId) public view returns (uint256){
+    function getReward(uint256 tokenId) public view returns (uint256){
         return _reward[tokenId];
     }
     
     function daysLeft(uint256 tokenId) public view returns (uint256){
        uint256 lockEnd = _lockStart[tokenId] + _lockDays[tokenId]*(1 days);
-       if(block.timestamp>lockEnd) return 0;
-       return (lockEnd-block.timestamp)/(1 days);
+       if(_blockTime()>lockEnd) return 0;
+       return (lockEnd-_blockTime())/(1 days);
     }
     
     function lockInfo(uint256 tokenId) public view returns (uint256, uint256, uint256){
-        return (lockedAmount(tokenId), reward(tokenId), daysLeft(tokenId));
+        return (lockedAmount(tokenId), getReward(tokenId), daysLeft(tokenId));
     }
     
     function release(uint256 tokenId) public {
@@ -61,6 +65,27 @@ contract LockedELYS is ERC721, Ownable {
         _reward[tokenId] = 0;
         _token.safeTransfer(ownerOf(tokenId), bal + rwrd);
         _burn(tokenId);
+    }
+    
+    function emergencyRelease(uint256 tokenId) public {
+        require(ownerOf(tokenId)==msg.sender);
+        require(_lockedAmount[tokenId]>0);
+        uint256 bal = _lockedAmount[tokenId];
+        _lockedAmount[tokenId] = 0;
+        uint256 rwrd = _reward[tokenId];
+        _reward[tokenId] = 0;
+        _token.safeTransfer(ownerOf(tokenId), bal);
+        _token.safeTransfer(_factory, rwrd);
+        _burn(tokenId);
+    }
+    
+    function _blockTime() private view returns (uint256){
+        //return block.timestamp;
+        return _timestamp;
+    }
+    
+    function _inc(uint256 numDays) public {
+        _timestamp += numDays * (1 days);
     }
     
 }
